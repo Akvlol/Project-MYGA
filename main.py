420e43287a5f67c9b10572550ba5edb4e17192f7
Akvlol <ahk811469@gmail.com>
2026-09-12 16:46:31 +0700

    Thêm option clear vào các menu

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
 from play_list import Playlist
 from play_manager import PlaybackManager
 from tool.input_tool import InputTool
+import subprocess
+import platform
 
 #------------------------------------------------------------------------------
 # Edit mode
@@ -16,13 +18,14 @@ Edit mode:
 [u] - Update an existing song
 [x] - Delete a song
 [s] - Save the playlist to a file
+[clear] - Clear the screen
 [exit] - Back to the main menu
 """)
 def edit_playlist(playlist):
     print_menu_edit()
     changes_made = False
     while True:
-        choice = InputTool.valid_value("<edit># ", ['h','f', 'a', 'd', 'D', 'u', 'x', 's', 'exit'], "Invalid choice. Press 'h' for help.\n")
+        choice = InputTool.valid_value("<edit># ", ['h','f', 'a', 'd', 'D', 'u', 'x', 's', 'clear', 'exit'], "Invalid choice. Press 'h' for help.\n")
         match choice:
             case 'h':
                 print_menu_edit()
@@ -41,6 +44,8 @@ def edit_playlist(playlist):
             case 's':
                 playlist.save_playlist_to_json()
                 changes_made = False
+            case 'clear':
+                subprocess.run('cls' if platform.system() == 'Windows' else 'clear', shell=True)
             case 'exit':
                 if changes_made:
                     if InputTool.yes_or_no("Save before quitting?"):
@@ -65,6 +70,7 @@ Play mode:
 [n] - Play the next song
 [p] - Play the previous song
 [g] - Get current song info
+[clear] - Clear the screen
 [exit] - Stop & back to the main menu
 """)
 def play_playlist(manager):
@@ -72,7 +78,7 @@ def play_playlist(manager):
     while True:
         song_name = manager.get_current_song().title.split(" - ")[0]
         choice = InputTool.valid_value(f"<playing: {song_name} ># " if len(song_name) < 36 else f"<playing: {song_name[:33]}... ># ",
-                                       ['h', 's', 'z', 'n', 'p', 'x', 'g', 'exit'],
+                                       ['h', 's', 'z', 'n', 'p', 'x', 'g', 'exit', 'clear'],
                                        "Invalid choice. Press 'h' for help.\n",
                                        continue_if_empty=False)
         if choice == "":
@@ -93,6 +99,8 @@ def play_playlist(manager):
                 manager.previous()
             case 'g':
                 manager.get_current_song_info()
+            case 'clear':
+                subprocess.run('cls' if platform.system() == 'Windows' else 'clear', shell=True)
             case 'exit':
                 manager.stop()
                 print()
@@ -109,14 +117,17 @@ def main():
         print("""Main menu:
 [e] - Edit the playlist
 [p] - Play the playlist
+[clear] - Clear the screen
 [quit] - Quit the program
 """)
-        choice = InputTool.valid_value("<main># ", ['e', 'p', 'quit'], "Invalid choice. Please enter 'e', 'p', or 'quit'.\n")
+        choice = InputTool.valid_value("<main># ", ['e', 'p', 'clear', 'quit'], "Invalid choice. Please enter 'e', 'p', or 'quit'.\n")
         match choice:
             case 'e':
                 edit_playlist(playlist)
             case 'p':
                 play_playlist(manager)
+            case 'clear':
+                subprocess.run('cls' if platform.system() == 'Windows' else 'clear', shell=True)
             case 'quit':
                 if InputTool.yes_or_no("Quit?"):
                     print("Exiting the program.")
